f0c6f82c10401bb0e5b846d205e73769e65613bd
zhangbin <zhangbin02@sunland.com>
2020-03-09 11:18:52 +0800

    Initial Commit

---
+++ src/main/java/com/ruoyi/project/weixin/constant/WebSocketConstant.java
@@ -0,0 +1,11 @@
+package com.ruoyi.project.weixin.constant;
+
+/**
+ * @author JL
+ */
+public interface WebSocketConstant {
+
+	String USER_DESTINATION_PREFIX = "/weixin/";
+	String WX_MSG = "wx_msg";
+
+}
---
+++ src/main/java/com/ruoyi/project/weixin/entity/WxUserTagsDict.java
@@ -0,0 +1,9 @@
+package com.ruoyi.project.weixin.entity;
+
+import lombok.Data;
+
+@Data
+public class WxUserTagsDict {
+	private String name;
+	private Long value;
+}
