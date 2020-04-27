2ba80a6f7b4394403a7b10802fdd7487129d54b8
pengjizheng <pengjizheng@sunlands.com>
2020-04-27 11:28:03 +0800

    SQL

--- sql/release/v1.0.2-20200430.sql
+++ sql/release/v1.0.2-20200430.sql
@@ -1,3 +1,5 @@
+-- 备份表wx_activity_template_message wx_mp_template_message
+
 ALTER TABLE `wx_user`
 ADD COLUMN `app_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户所在公号APPID' AFTER `open_id`,
 DROP PRIMARY KEY,
