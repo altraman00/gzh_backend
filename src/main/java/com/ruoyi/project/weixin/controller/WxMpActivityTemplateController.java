db79ac27f9f80a35b1b45d4c401d68a19456bc85
谢昆 <xiekun@sunlands.com>
2020-05-20 16:47:03 +0800

    修改公众号可以绑定多个活动模版

---
+++ src/main/java/com/ruoyi/project/weixin/controller/WxMpActivityTemplateController.java
@@ -0,0 +1,21 @@
+package com.ruoyi.project.weixin.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+import com.ruoyi.framework.web.controller.BaseController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author xiekun
+ * @since 2020-05-20
+ */
+@RestController
+@RequestMapping("/wx-mp-activity-templete")
+public class WxMpActivityTemplateController extends BaseController {
+
+}
---
+++ src/main/java/com/ruoyi/project/weixin/mapper/WxMpActivityTempleteMapper.java
@@ -0,0 +1,16 @@
+package com.ruoyi.project.weixin.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ruoyi.project.weixin.entity.WxMpActivityTemplete;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author xiekun
+ * @since 2020-05-20
+ */
+public interface WxMpActivityTempleteMapper extends BaseMapper<WxMpActivityTemplete> {
+
+}
