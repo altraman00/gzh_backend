d1c3a54026c066bbecd3dbf203ca66a86d2003e4
谢昆 <xiekun@sunlands.com>
2020-05-26 18:30:08 +0800

    获取用户的助力队友列表

--- src/main/java/com/ruoyi/project/weixin/controller/yunchan/yunchan001/WxMpYunchan001HelpUserRecordController.java
+++ src/main/java/com/ruoyi/project/weixin/controller/yunchan/yunchan001/WxMpYunchan001HelpUserRecordController.java
@@ -1,10 +1,23 @@
 package com.ruoyi.project.weixin.controller.yunchan.yunchan001;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.ruoyi.framework.web.controller.BaseController;
+import com.ruoyi.framework.web.domain.AjaxResult;
+import com.ruoyi.project.weixin.entity.WxUser;
+import com.ruoyi.project.weixin.entity.yunchan.yunchan001.WxMpYunchan001HelpUserRecord;
+import com.ruoyi.project.weixin.service.WxUserService;
+import com.ruoyi.project.weixin.service.yunchan.yunchan001.IWxMpYunchan001HelpUserRecordService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -15,7 +28,28 @@ import com.ruoyi.framework.web.controller.BaseController;
  * @since 2020-05-25
  */
 @RestController
-@RequestMapping("/wx-mp-yunchan001-help-user-record")
+@RequestMapping("/open/mp/yunchan001/user/record")
 public class WxMpYunchan001HelpUserRecordController extends BaseController {
+
+    @Autowired
+    private WxUserService wxUserService;
+
+    @Autowired
+    private IWxMpYunchan001HelpUserRecordService wxMpYunchan001HelpUserRecordService;
+
+    @ApiOperation("获取用户的助力队友列表")
+    @ApiImplicitParam(name = "openId", value = "openId", required = true, paramType = "String")
+    @GetMapping("/")
+    public AjaxResult getUserStageStatus(@RequestParam(value = "openId") String openId) {
+
+        WxUser one = wxUserService.getOne(Wrappers.<WxUser>lambdaQuery().eq(WxUser::getOpenId, openId));
+        List<WxMpYunchan001HelpUserRecord> list = wxMpYunchan001HelpUserRecordService.list(Wrappers.<WxMpYunchan001HelpUserRecord>lambdaQuery()
+                .eq(WxMpYunchan001HelpUserRecord::getInviteWxUserId, one.getId())
+                .orderByAsc(WxMpYunchan001HelpUserRecord::getCreateTime)
+                .last("limit 0,3"));
+        List<String> collect = list.stream().map(t -> t.getHelpWxUserId()).collect(Collectors.toList());
+        List<WxUser> helpUsers = wxUserService.lambdaQuery().in(true,WxUser::getId, collect).list();
+        return AjaxResult.success(helpUsers);
+    }
 
 }
